9fe5779d349afe729e42e9615cf801344428c426
Nathan Dunn <ndunn@me.com>
2020-12-01 12:22:52 -0800

    upped analysis limit

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import * as bodyParser from 'body-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // the next two lines did the trick
-  app.use(bodyParser.json({ limit: '50mb' }))
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+  app.use(bodyParser.json({ limit: '100mb' }))
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
   app.enableCors()
   await app.listen(3001)
 }

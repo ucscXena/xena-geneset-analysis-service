ace9f5c800728cd31fe9565d2d2a1f9667365a96
Nathan Dunn <ndunn@me.com>
2020-12-03 12:10:08 -0800

    added routes

--- src/analysis-results/analysis-results.controller.ts
+++ src/analysis-results/analysis-results.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {AnalysisResultsService} from "./analysis-results.service";
 import {AnalysisResults} from "./analysis-results.entity";
 
@@ -12,4 +12,20 @@ export class AnalysisResultsController {
     return this.analysisResultsService.findAll();
   }
 
+  @Post('create')
+  async create(@Body() analysisResultData: AnalysisResults): Promise<any> {
+    return this.analysisResultsService.create(analysisResultData);
+  }
+
+  @Put(':id/update')
+  async update(@Param('id') id, @Body() analysisResultsData: AnalysisResults): Promise<any> {
+    analysisResultsData.id = Number(id);
+    console.log('Update #' + analysisResultsData.id)
+    return this.analysisResultsService.update(analysisResultsData);
+  }
+
+  @Delete(':id/delete')
+  async delete(@Param('id') id): Promise<any> {
+    return this.analysisResultsService.delete(id);
+  }
 }
